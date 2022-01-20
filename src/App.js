40ac45d073afc364efe9ec7f3867a494c535e8b3
JoannaCoPo <jococopo@gmail.com>
2022-01-20 15:39:23 -0700

    Save

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 const Pet = () => { //"pet stamp"
     return React.createElement("div", {}, [  // array - I have a div and within that div there's going to be 3 other elms
         React.createElement("h2", {}, "Tony"),
-        React.createElement("h2", {}, "Cat"),
-        React.createElement("h2", {}, "Black MeowMeow"),
+        React.createElement("h3", {}, "Cat"),
+        React.createElement("h3", {}, "Black MeowMeow"),
     ])
 }
 
@@ -12,9 +12,9 @@ const App = () => {                  // function that returns the results of Rea
         {},                        // then stamps any added attributes (if object weren't empty)
         [
             React.createElement("h1", {}, "Adopt Me!"), // {} == empty object, so you can put put in attributes (class name, style, etc.) that you want passed on to that element 
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet),
+            React.createElement(Pet),
+            React.createElement(Pet), // 6 h3's will be outputted to the DOM (2 per Pet * the 3x in App)
         ]
     );
 };
