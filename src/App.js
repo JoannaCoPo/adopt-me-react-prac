862d8689e633783a7f779adce98f66ccfc7d2214
JoannaCoPo <jococopo@gmail.com>
2022-01-24 15:19:07 -0700

    Move pet component to own file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-const Pet = (props) => {
-  //"pet stamp"
-  return React.createElement("div", {}, [
-    // array - I have a div and within that div there's going to be 3 other elms
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";  
+import Pet from './Pet'
 
 const App = () => {
   // function that returns the results of React.createElement: A REACT COMPONENT (App as a rubber stamp)
